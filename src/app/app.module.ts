dafd72d7bec11b831062f71050aa0b8bc2b820fa
HotPotatoes86 <rcharpentier10@gmail.com>
2019-05-21 14:48:48 +0200

    4 files modified, 5 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ChildComponent } from './child/child.component';
+import { CadreComponent } from './cadre/cadre.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, ChildComponent, CadreComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/cadre/cadre.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadre',
+  template: `
+      <div class="bordure">
+        <h1>
+          <ng-content></ng-content>     
+        </h1>
+      </div>
+  `,
+  styles: [`
+    h1 {
+      color : darkmagenta;
+    }
+    .bordure {
+      border: 1px solid darkmagenta;
+    }
+  `]
+})
+export class CadreComponent  {
+
+}
